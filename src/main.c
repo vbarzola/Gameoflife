49c6592515cc6c5fc66d91a0d64c52d642c125f9
AlexVelezLl <alanvele@espol.edu.ec>
2020-09-21 19:52:15 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/main.c
+++ src/main.c
@@ -1,16 +1,51 @@
-#include<stdio.h>
-//#include<windows.h>
-#include<stdlib.h>
-#include "util.h"
-int main(){
-    char **matriz = malloc(2*sizeof(char*));
-    matriz[0] = malloc(2*sizeof(char));
-    matriz[1] = malloc(2*sizeof(char));
-    matriz[0][0] = 1;
-    matriz[0][1]=1;
-    dibujar_grilla(matriz,2,2);
-    free(matriz[0]);
-    free(matriz[1]);
-    free(matriz);
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/util.h"
 
+int filas;
+int columnas;
+int generaciones;
+int tiempo;
+int celulas;
+
+int main(int argc, char **argv)
+{
+    int index;
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt(argc, argv, "f:c:g:s:i:")) != -1)
+        switch (c)
+        {
+        case 'f':
+            filas = optarg;
+            break;
+        case 'c':
+            columnas = optarg;
+            break;
+        case 'g':
+            generaciones = optarg;
+            break;
+        case 's':
+            tiempo = optarg;
+            break;
+        case 'i':
+            celulas = optarg;
+            break;
+        case '?':
+            if (optopt == 'c')
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint(optopt))
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf(stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+            return 1;
+        default:
+            abort();
+        }
+
+    return 0;
 }
