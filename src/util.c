4c2c64e3e054e38df4ed19d49b456977416aaf0d
vbarzola <vbarzola@espol.edu.ec>
2020-09-21 21:19:11 -0500

    actualiza matriz

--- src/util.c
+++ src/util.c
@@ -6,6 +6,9 @@
 #include "vida.h"
 
 
+char** crear_matriz();
+void destruir_matriz();
+juego_de_vida vida;
 void dibujar_grilla(char **matriz, int fil, int col){
 	printf("\e[1;1H\e[2J");
 	char *linea = malloc(col + 1);	//Char nulo al final
@@ -49,9 +52,43 @@ void llenar_matriz_azar(char **grilla, int fil, int col, int cantidad){
 	}
 }
 
+
+
+
 /**
  * Actualiza la matriz de vida.
  */
 void actualizar(){
+	char** nuevaMatriz = crear_matriz();
+	for(int i=0;i<vida.filas;i++){
+		for(int j=0;j<vida.columnas;j++){
+			int c = contar_alrededor(i,j);
+			if(vida.tablero[i][j]==1){
+				if(c<=1||c>=4) nuevaMatriz[i][j]=0;
+				else nuevaMatriz[i][j]=1;
+			}else{
+				if(c==3) nuevaMatriz[i][j]=1;
+			}
+			
+		}
+	
+	}
 
+	destruir_matriz();
+	vida.tablero= nuevaMatriz;
+
+
+}
+
+int contar_alrededor(int i, int j){
+	char ** tablero = vida.tablero;
+	int cont=0;
+	for(int x=i-1;x<i+2;x++){
+		for(int y=j-1;y<j+2;y++){
+			if((i!=x)&&(j!=y)&&(x>=0&&x<vida.filas)&&(y>=0&&y<vida.columnas)&&(tablero[x][y]==1)){
+			       	cont++;
+			}		
+		}
+	}
+	return cont;
 }
