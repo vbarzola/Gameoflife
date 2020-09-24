f40ad50d45bac71a98fb0cc1118223a3c4379ade
AlexVelezLl <alanvele@espol.edu.ec>
2020-09-23 21:18:13 -0500

    Estructura de estadisticas

--- include/vida.h
+++ include/vida.h
@@ -2,12 +2,23 @@
 #ifndef VIDA_H
 #define VIDA_H
 
+typedef struct stats{
+	int n_generacion;
+	int celulas_vivas;
+	int celulas_muertas;
+	int celulas_nacidas;
+	int celulas_fallecidas;
+	int promedio_celulas_vivas;
+	int promedio_celulas_muertas;
+} stats;
+
 typedef struct vida{
-	char **tablero;	
+	char **tablero;
 	long filas;
 	long columnas;
 	long tiempo_sleep;
-	long generaciones;	
+	long generaciones;
+	stats stats;
 } juego_de_vida;
 
-#endif 
+#endif
